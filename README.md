22b71c1df7eb5fbdcde6ac748b1f2f797fb8abbf
Filip <flipchan@riseup.net>
2024-01-13 18:38:00 +0100

    actix web base

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# rusty-substrate-api
-A rust http json api for substrate 
 
 
-Hacky POC so far, do not use in prod
+Rust re-write of: https://github.com/XcmSend/api
+
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,72 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, post, web, App,
+     HttpServer, HttpResponse, middleware}; // , Result
 
+use actix_web::http::header::ContentType;
 
-#[get("/hello/{name}")]
-async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {name}!")
+#[get("/")]
+async fn info() -> HttpResponse {
+    HttpResponse::Ok().body("Documentation: https://xcmsend.github.io/api/index.html")
+
+//    format!("Documentation: https://xcmsend.github.io/api/index.html")
 }
 
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> HttpResponse {
+    HttpResponse::Ok().body("Todo!")
+}
+
+// open channels, list open ingoing and outgoing hrmp channels for paraid
+#[post("/polkadot/openchannels")]
+async fn dot_openchannels() -> HttpResponse {
+    HttpResponse::Ok().body("Todo!")
+}
+
+// broadcast input: {chain: 'hydradx', tx: ''}
+#[post("/broadcast")]
+async fn broadcast_tx() -> HttpResponse {
+    HttpResponse::Ok().body("Todo!")
+}
+
+#[post("/saveUrl")]
+async fn save_url() -> HttpResponse {
+    HttpResponse::Ok().body("Todo!")
+}
+
+#[get("/getUrl/{name}")]
+async fn get_url(name: web::Path<String>) -> HttpResponse {
+    let fluff = format!("Todo {name}!");
+
+    HttpResponse::Ok().body("Todo!")
+}
+
+#[post("/xcm-asset-transfer")]
+async fn xcm_asset_transfer() -> HttpResponse {
+    HttpResponse::Ok().body("Todo!")
+}
+
+
+
+
+async fn indexme() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type(ContentType::plaintext())
+        .insert_header(("X-Hdr", "sample"))
+        .body("data")
+}
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     println!("running http server http://127.0.0.1:8080");
     HttpServer::new(|| {
-        App::new().service(greet)
+        App::new()
+            .wrap(middleware::Compress::default())
+            .service(xcm_asset_transfer)
+            .service(get_url)
+            .service(save_url)
+            .service(broadcast_tx)
+            .service(dot_openchannels)
+            .service(info)
+
     })
     .bind(("127.0.0.1", 8080))?
     .run()
